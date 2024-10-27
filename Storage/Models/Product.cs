cc357514546891a2e900e1485bebe2030782a95b
Andreas Johansson <glanshammar70@gmail.com>
2024-10-27 19:41:42 +0100

    Added restrictions for Product

--- Storage/Models/Product.cs
+++ Storage/Models/Product.cs
@@ -1,14 +1,34 @@
-﻿namespace Storage.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1, 1000000)]
         public int Price { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime Orderdate { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Category { get; set; }
+
+        [Required]
+        [StringLength(10)]
         public string Shelf { get; set; }
+
+        [Range(0, 10000)]
         public int Count { get; set; }
+
+        [StringLength(500)]
         public string Description { get; set; }
     }
 }
